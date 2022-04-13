c37956dd702c6d40379401beff233ddce9499f28
nazmul-2020 <nazmulcpi2012@gmail.com>
2022-04-13 12:55:39 +0600

    creat sign up page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,20 +7,29 @@ const Login = () => {
         <div className='form-container'>
             <div>
                 <form action="">
-                   
-                        <h2 className='form-title'>Login</h2>
-                        <div className="input-grope">
-                            <label htmlFor="email">Email</label>
-                            <input type="email" name="email" id="" placeholder='Write Your Email' />
-                        </div>
-                        <div className="input-grope">
-                            <label htmlFor="password">Password</label>
-                            <input type="password" name="password" id="" placeholder='Enter Your password' />
-                        </div>
-                        <input className="form-submit" type="submit" value="Login" />
-                    
-                    <p>New to Ema-john? <Link className='form-link' to='/signup'>Create New Account</Link></p>
+
+                    <h2 className='form-title'>Login</h2>
+                    <div className="input-grope">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" id="" placeholder='Write Your Email' required />
+                    </div>
+                    <div className="input-grope">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" id="" placeholder='Enter Your password' required/>
+                    </div>
+                    <input className="form-submit" type="submit" value="Login" />
                 </form>
+                <p>New to Ema-john? <Link className='form-link' to='/signup'>Create New Account</Link></p>
+                
+                <div className='ddd'>
+                    <hr className='ss' />
+                    <p>or</p>
+                    <hr className='ss' />
+                </div>
+
+                <div>
+                    <input className='form-submit-google' type="submit" value="Continue with Google" />
+                </div>
             </div>
         </div>
     );
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './SignUp.css'
 
 const SignUp = () => {
+    
     return (
-        <div>
-            <h2>This is SignUp </h2>
+        <div className='form-container'>
+            <div>
+                <form action="">
+
+                    <h2 className='form-title'>Sign Up</h2>
+                    <div className="input-grope">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" id="" placeholder='Write Your Email'required />
+                    </div>
+                    <div className="input-grope">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" id="" placeholder='Enter Your password'required />
+                    </div>
+                    <div className="input-grope">
+                        <label htmlFor="confirm-password">Confirm Password</label>
+                        <input type="password" name="confirm-password" id="" placeholder='Enter Your password' required />
+                    </div>
+                    <input className="form-submit" type="submit" value="Sign Up" />
+                </form>
+                <p>Already have an account?<Link className='form-link' to='/login'>Login</Link></p>
+                
+                <div className='ddd'>
+                    <hr className='ss' />
+                    <p>or</p>
+                    <hr className='ss' />
+                </div>
+                
+                <div>
+                    <input className='form-submit-google' type="submit" value="Continue with Google" />
+                </div>
+            </div>
         </div>
     );
 };
