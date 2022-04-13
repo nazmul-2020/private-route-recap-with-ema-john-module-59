6ca656b058389e18021634f3be14de2757816a25
nazmul-2020 <nazmulcpi2012@gmail.com>
2022-04-14 05:16:13 +0600

    add login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import './Login.css';
 
 const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    const navigate = useNavigate()
+
+    const handelEmailBlur = event => {
+        setEmail(event.target.value)
+    }
+
+    const handelPasswordBlur = event => {
+        setPassword(event.target.value)
+    }
+
+    if (user) {
+        navigate('/shop')
+    }
+
+    const handelUserSignIn = event => {
+        event.preventDefault();
+        signInWithEmailAndPassword(email, password)
+    }
+
     return (
         <div className='form-container'>
             <div>
-                <form action="">
+                <form onSubmit={handelUserSignIn}>
 
                     <h2 className='form-title'>Login</h2>
                     <div className="input-grope">
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" id="" placeholder='Write Your Email' required />
+                        <input onBlur={handelEmailBlur} type="email" name="email" id="" placeholder='Write Your Email' required />
                     </div>
                     <div className="input-grope">
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id="" placeholder='Enter Your password' required/>
+                        <input onBlur={handelPasswordBlur} type="password" name="password" id="" placeholder='Enter Your password' required />
+                        <p style={{ color: 'red' }}>{error?.message}</p>
+                        {
+                            loading && <p>Loading...</p>
+                        }
                     </div>
                     <input className="form-submit" type="submit" value="Login" />
                 </form>
                 <p>New to Ema-john? <Link className='form-link' to='/signup'>Create New Account</Link></p>
-                
+
                 <div className='ddd'>
                     <hr className='ss' />
                     <p>or</p>
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,37 +1,72 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate, } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 import './SignUp.css'
 
 const SignUp = () => {
-    
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [error, setError] = useState('')
+    const navigate = useNavigate()
+
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth)
+
+    const handelEmailBlur = event => {
+        setEmail(event.target.value)
+    }
+
+    const handelPasswordBlur = event => {
+        setPassword(event.target.value)
+    }
+
+    const handelConfirmPassword = event => {
+        setConfirmPassword(event.target.value)
+    }
+    if (user) {
+        navigate('/')
+    }
+
+    const handelCreatUser = event => {
+        event.preventDefault()
+        if (password !== confirmPassword) {
+            setError("your password is did not match")
+            return;
+        }
+
+        createUserWithEmailAndPassword(email, password)
+    }
+
     return (
         <div className='form-container'>
             <div>
-                <form action="">
+                <form onSubmit={handelCreatUser}>
 
                     <h2 className='form-title'>Sign Up</h2>
                     <div className="input-grope">
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" id="" placeholder='Write Your Email'required />
+                        <input onBlur={handelEmailBlur} type="email" name="email" id="" placeholder='Write Your Email' required />
                     </div>
                     <div className="input-grope">
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id="" placeholder='Enter Your password'required />
+                        <input onBlur={handelPasswordBlur} type="password" name="password" id="" placeholder='Enter Your password' required />
                     </div>
                     <div className="input-grope">
                         <label htmlFor="confirm-password">Confirm Password</label>
-                        <input type="password" name="confirm-password" id="" placeholder='Enter Your password' required />
+                        <input onBlur={handelConfirmPassword} type="password" name="confirm-password" id="" placeholder='Enter Your password' required />
                     </div>
+                    <p style={{ color: 'red' }}>{error}</p>
                     <input className="form-submit" type="submit" value="Sign Up" />
                 </form>
                 <p>Already have an account?<Link className='form-link' to='/login'>Login</Link></p>
-                
+
                 <div className='ddd'>
                     <hr className='ss' />
                     <p>or</p>
                     <hr className='ss' />
                 </div>
-                
+
                 <div>
                     <input className='form-submit-google' type="submit" value="Continue with Google" />
                 </div>
